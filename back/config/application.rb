ea2d9fc5acefc123e7c38b18f646e0c8924d24ec
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-07-05 22:21:07 +0900

    back_test

--- back/config/application.rb
+++ back/config/application.rb
@@ -38,3 +38,11 @@ module App
     config.api_only = true
   end
 end
+
+config.generators do |g|
+  g.test_framework :rspec, 
+        view_specs: false, 
+        helper_specs: false, 
+        controller_specs: false, 
+        routing_specs: false
+end
