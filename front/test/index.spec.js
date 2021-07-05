1e45f44c7ff2d396e3d97d8b7e34e60c28bd7cc2
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-07-05 20:21:21 +0900

    front_test

---
+++ front/test/index.spec.js
@@ -0,0 +1,9 @@
+import Index from '../pages/index.vue'
+
+describe('pages/index.vue', () => {
+  it('has a default value', () => {
+    const defaultData = Index.data()
+    // subTitle の初期値をテストする
+    expect(defaultData.subTitle).toBe("MAKE YOUR FOOD HAPPY!!")
+  })
+})
